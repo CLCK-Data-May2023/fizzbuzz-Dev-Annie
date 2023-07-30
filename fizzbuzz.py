fd0f3335955bc176af9c5ba45751bb5352ceb2e7
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-07-30 17:58:47 -0400

    commit2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
-
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FIZZBUZZ")
+        continue
+    elif num %3 == 0:
+        print("FIZZ") 
+        continue
+    elif num % 5 == 0:
+        print("BUZZ")
+        continue
+    print(num)
